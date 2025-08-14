9980e337892b4ecac7293c35c68c133c5b81b45e
NidhiLavand <159647817+NidhiLavand@users.noreply.github.com>
2025-08-14 15:35:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(
     description="Estimate power usage, energy usage and carbon emissions of Python programs",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/Power_Profile",
+    url="https://github.com/NidhiLavand/PowerProfile.git",
     packages=find_packages(),
     install_requires=[
         "psutil>=5.9.0"
     ],
     python_requires=">=3.7",
 )
+
